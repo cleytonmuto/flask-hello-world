ef5ba8a306a44306014bc3a6c1169e6ea40bc2ba
Cleyton Muto <cleytonmuto@gmail.com>
2024-03-06 09:36:09 -0300

    chore: test several routes

--- api/index.py
+++ api/index.py
@@ -2,9 +2,14 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route("/")    
+@app.route("/")   
 def index():
     return '<h1 style="color:blue;font-family=Arial">Hello, world!</h1>', 200
+
+for hash in range(1,5):
+    @app.route("/ABCD000" + hash)
+    def index():
+        return f'<h1 style="color:blue;font-family=Arial">Hello, world {hash}!</h1>', 200
 
 app.errorhandler(404)
 def page_not_found(e):
