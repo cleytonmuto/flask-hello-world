b2a13500e4aee67bd8e5ec34b5f956c64728f69d
Cleyton Muto <cleytonmuto@gmail.com>
2024-03-06 09:57:07 -0300

    chore: reconfigured root route

--- api/index.py
+++ api/index.py
@@ -2,13 +2,11 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route("/")   
-def index():
-    return '<h1 style="color:blue;font-family=Arial">Hello, world!</h1>', 200
-
-@app.route("/check/<page>")
-def check(page):
+@app.route("/<page>")   
+def index(page):
+    # return '<h1 style="color:blue;font-family=Arial">Hello, world!</h1>', 200
     return f'<h1 style="color:red;font-family=Arial">Certificado ABCD000{page}!</h1>', 200
+
 
 @app.errorhandler(404)
 def page_not_found(e):
