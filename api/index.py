70ef631cb2570b20645425f909f264ab27b48548
Cleyton Muto <cleytonmuto@gmail.com>
2024-03-06 09:55:13 -0300

    fix: new route test

--- api/index.py
+++ api/index.py
@@ -6,11 +6,10 @@ app = Flask(__name__)
 def index():
     return '<h1 style="color:blue;font-family=Arial">Hello, world!</h1>', 200
 
-@app.route("/<page>")
-def index(page):
-    return f'<h1 style="color:red;font-family=Arial">Hello, world {page}!</h1>', 200
+@app.route("/check/<page>")
+def check(page):
+    return f'<h1 style="color:red;font-family=Arial">Certificado ABCD000{page}!</h1>', 200
 
 @app.errorhandler(404)
 def page_not_found(e):
     return jsonify({"status": 404, "message": "Not Found"}), 404
-
