91a317f0d939a37aea502be45f17f84c68abaf07
Cleyton Muto <cleytonmuto@gmail.com>
2024-03-06 09:27:43 -0300

    fix: fixed api/index.py folder

---
+++ api/index.py
@@ -0,0 +1,12 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route("/")    
+def index():
+    return '<h1 style="color:blue;font-family=Arial">Hello, world!</h1>', 200
+
+app.errorhandler(404)
+def page_not_found(e):
+    return jsonify({"status": 404, "message": "Not Found"}), 404
+
